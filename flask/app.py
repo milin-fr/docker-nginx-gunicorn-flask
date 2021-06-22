100d64471e0e4a37f4f39f9cac2757e096560674
unknown <m.ilin.fr@gmail.com>
2021-06-22 14:30:38 +0200

    adding redis

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 import os
 import urllib
+import redis
+
+
+target = os.getenv("target")
+REDIS_URL = os.getenv("REDIS_URL")
 
 app = Flask(__name__)
 
-target = os.getenv("target")
+app.redis_storage = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 
 @app.route("/")
@@ -12,6 +17,20 @@ def index():
     with urllib.request.urlopen(target) as response:
         html = response.read()
     return html
+
+
+@app.route("/redis/get/<something>")
+def redis_get(something):
+    result = app.redis_storage.get(something)
+    if not result:
+        result = "nope"
+    return result
+
+
+@app.route("/redis/set/<something>")
+def redis_set(something):
+    app.redis_storage.set(something, 'bingo', 900)
+    return something
 
 
 if __name__ == "__main__":
